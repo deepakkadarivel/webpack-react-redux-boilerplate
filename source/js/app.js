ab4cd7945a888996af1fd3ff0552e9010a1116e1
Deepak K V <deepakkv@in-deepakkv.local>
2017-06-11 20:08:44 +0530

    [Deepak] Webpack set up for React Redux using Babel

---
+++ source/js/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React is up and running in webpack server.</h1>
+      </div>
+    )
+  }
+}
+
+export default App
